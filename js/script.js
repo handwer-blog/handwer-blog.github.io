0561469088750308f26103ab51eded62c5eb73f5
HandwerSTD <1875876967@qq.com>
2020-12-03 11:45:35 +0800

    Site updated: 2020-12-03 11:45:07

--- js/script.js
+++ js/script.js
@@ -17,11 +17,13 @@ console.log("%c Github %c", "background:#333333; color:#ffffff", "", "https://gi
       $(".fab-up").addClass("fab-up-active");
       $(".fab-plus").addClass("fab-plus-active");
       $(".fab-daovoice").addClass("fab-daovoice-active");
+      $(".fab-tencent-chao").addClass("fab-tencent-chao-active");
     },
     freezeFab: function () {
       $(".fab-up").removeClass("fab-up-active");
       $(".fab-plus").removeClass("fab-plus-active");
       $(".fab-daovoice").removeClass("fab-daovoice-active");
+      $(".fab-tencent-chao").removeClass("fab-tencent-chao-active");
     },
     showFab: function () {
       $(".fab").removeClass("fab-hide").addClass("fab-show");
@@ -85,25 +87,27 @@ console.log("%c Github %c", "background:#333333; color:#ffffff", "", "https://gi
       },
     },
     motto: function () {
-      if (CONFIG.preview.motto.api) {
-        $.get(CONFIG.preview.motto.api, function (data) {
-          if (data) {
-            $("#motto").text(data);
-          }
-        });
-      }
+      if (!CONFIG.preview.motto.api) return;
+      var data_contents = CONFIG.preview.motto.data_contents && JSON.parse(CONFIG.preview.motto.data_contents);
+      $.get(CONFIG.preview.motto.api, function (result) {
+        if (data_contents.length > 0) {
+          data_contents.forEach(function (item) {
+            result = result[item];
+          });
+        }
+        result && $("#motto").text(result);
+      });
     },
     background: function () {
-      if (CONFIG.preview.background.api) {
-        $(".preview-image").css("background-image", "url(" + CONFIG.preview.background.api + ")");
-      }
+      if (!CONFIG.preview.background.api) return;
+      $(".preview-image").css("background-image", "url(" + CONFIG.preview.background.api + ")");
     }
   }
 
   var action = {
     smoothScroll: function () {
       // a[href *=#], area[href *=#]
-      $(".smooth-scroll").click(function () {
+      $(".smooth-scroll, .toc-link").click(function () {
         if (location.pathname.replace(/^\//, "") == this.pathname.replace(/^\//, "") && location.hostname == this.hostname) {
           var $target = $(decodeURIComponent(this.hash));
           $target = $target.length && $target || $("[name=" + this.hash.slice(1) + "]");
@@ -132,9 +136,8 @@ console.log("%c Github %c", "background:#333333; color:#ffffff", "", "https://gi
       });
       $(".fab-daovoice").on("click", function () {
         daovoice('openMessages');
-        fn.freezeFab();
       });
-      $(".fab-up .fab-daovoice").on("click", function () {
+      $(".fab-up, .fab-daovoice, fab-tencent-chao").on("click", function () {
         fn.freezeFab();
       });
       if (CONFIG.fab.always_show) {
@@ -171,9 +174,7 @@ console.log("%c Github %c", "background:#333333; color:#ffffff", "", "https://gi
         });
       });
       $(document).on('pjax:complete', function () {
-        if (CONFIG.fancybox) {
-          action.fancybox();
-        }
+        CONFIG.fancybox && action.fancybox();
       });
     },
     donate: function () {
@@ -207,12 +208,8 @@ console.log("%c Github %c", "background:#333333; color:#ffffff", "", "https://gi
     },
     navbar: function () {
       $(window).resize(ZHAOO.utils.throttle(function () {
-        if (ZHAOO.utils.isDesktop()) {
-          fn.navbar.desktop();
-        }
-        if (ZHAOO.utils.isMobile() && !CONFIG.isHome) {
-          fn.navbar.mobile();
-        }
+        ZHAOO.utils.isDesktop() && fn.navbar.desktop();
+        (ZHAOO.utils.isMobile() && !CONFIG.isHome) && fn.navbar.mobile();
       }, 1000)).resize();
       $(".j-navbar-menu").on("click", function () {
         fn.showMenu();
@@ -241,6 +238,68 @@ console.log("%c Github %c", "background:#333333; color:#ffffff", "", "https://gi
       } else if (CONFIG.qrcode.type === 'image') {
         $("#qrcode-navbar").append('<img src="' + CONFIG.qrcode.image + '" alt="qrcode" />');
       }
+    },
+    toc: function () {
+      var current = [];
+      var titleList = new Map();
+      $("article .content h1,h2,h3,h4,h5,h6").each(function () {
+        var title = $(this).attr("id");
+        var height = $(this).offset().top;
+        titleList.set(height, title);
+      });
+      $(window).on("scroll", f);
+      function f() {
+        var height = $(this).scrollTop() || $(window).scrollTop();
+        for (var item of titleList) {
+          if (item[0] >= height) {
+            current = item;
+            break;
+          }
+        }
+        $(".toc-link").removeClass("active");
+        $(".toc-link[href='#" + current[1] + "']").addClass("active");
+      };
+      f();
+    },
+    scrollbar: function () {
+      var totalH = $(document).height();
+      var clientH = $(window).height();
+      $(window).on("scroll", f);
+      function f() {
+        var validH = totalH - clientH;
+        var scrollH = $(document).scrollTop();
+        var height = scrollH / validH * 100;
+        $(".j-scrollbar-current").css("height", height + '%');
+      }
+      f();
+      $(".j-scrollbar").mousedown(function (e) {
+        var scrollH = e.offsetY * totalH / 100;
+        $("html,body").animate({ scrollTop: scrollH }, 300);
+        $(document).mousemove(function (e) {
+          var scrollH = (1 - ((clientH - e.clientY) / clientH)) * totalH;
+          $("html,body").scrollTop(scrollH);
+          $("html,body").css({ "user-select": "none", "cursor": "move" });
+        });
+        $(document).mouseup(function () {
+          $(document).off('mousemove');
+          $("html,body").css({ "user-select": "auto", "cursor": "default" });
+        });
+      });
+    },
+    notification: function () {
+      if (!CONFIG.notification.list) return;
+      var page_white_list = CONFIG.notification.page_white_list && JSON.parse(CONFIG.notification.page_white_list);
+      var page_black_list = CONFIG.notification.page_black_list && JSON.parse(CONFIG.notification.page_black_list);
+      var path = window.location.pathname;
+      if ((page_white_list && page_white_list.indexOf(path) < 0) || (page_black_list && page_black_list.indexOf(path) >= 0)) return;
+      var delay = CONFIG.notification.delay;
+      var list = JSON.parse(CONFIG.notification.list);
+      var playList = list.filter(function (item) {
+        return item.enable && ZHAOO.utils.isDuringDate(item.startTime, item.endTime) && item;
+      });
+      playList.forEach(function (item) {
+        ZHAOO.zui.notification({ title: item.title, content: item.content, delay: delay });
+      });
     }
   }
 
@@ -252,27 +311,16 @@ console.log("%c Github %c", "background:#333333; color:#ffffff", "", "https://gi
     action.menu();
     action.scroolToTop();
     action.preview();
-    if (CONFIG.fancybox) {
-      action.fancybox();
-    }
-    if (CONFIG.pjax) {
-      action.pjax();
-    }
-    if (CONFIG.lazyload.enable) {
-      action.lazyload();
-    }
-    if (CONFIG.donate.enable) {
-      action.donate();
-    }
-    if (CONFIG.lazyload && CONFIG.fancybox) {
-      action.fixLazyloadFancybox();
-    }
-    if (CONFIG.carrier.enable) {
-      action.carrier();
-    }
-    if (CONFIG.qrcode.enable) {
-      action.qrcode();
-    }
+    CONFIG.fancybox && action.fancybox();
+    CONFIG.pjax && action.pjax();
+    CONFIG.lazyload.enable && action.lazyload();
+    CONFIG.donate.enable && action.donate();
+    (CONFIG.lazyload && CONFIG.fancybox) && action.fixLazyloadFancybox();
+    CONFIG.carrier.enable && action.carrier();
+    CONFIG.qrcode.enable && action.qrcode();
+    CONFIG.toc.enable && action.toc();
+    CONFIG.scrollbar.model === 'simple' && action.scrollbar();
+    CONFIG.notification.enable && action.notification();
   });
 
 })(jQuery);
